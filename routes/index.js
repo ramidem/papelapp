3fac68cc3e3c32e38a3abb1f29b3d675243ae54d
Med Aduh <im.ramidem@gmail.com>
2021-11-21 22:31:37 +0800

    update index, remote users routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  res.render('index', { title: 'Papel.app' });
 });
 
 module.exports = router;
+
--- src/app.js
+++ src/app.js
@@ -5,22 +5,20 @@ import logger from 'morgan';
 import path from 'path';
 
 import indexRouter from'../routes/index';
-import usersRouter from'../routes/users';
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
